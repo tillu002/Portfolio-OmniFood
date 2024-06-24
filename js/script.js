0497ef23dcde3762e03e8f9c825257b14f6f78f9
Pavan  Sohith <121672097+tillu002@users.noreply.github.com>
2024-06-24 13:57:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ console.log("Hello world!");
 
 const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
 
 // h1.addEventListener("click", function () {
 //   h1.textContent = myName;
